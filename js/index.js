b7b2925d1daa67018ef37160f8e22c6096af7487
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-05-08 18:38:36 -0400

    Add subheader + update link popout / styling

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 //Use query selector to fetch the page header
 const header = document.querySelector('h1');
+const subheader = document.querySelector('h2');
 
 //Add an event listener for when the window is scrolled
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 600) {
         //If the offset reaches below 600, change the inner HTML
         header.innerHTML = "fridays at 7pm";
+        subheader.innerHTML = "may to august '23"
     } else {
         header.innerHTML = "the soul kitchen";
+        subheader.innerHTML = "by alex taveras-crespo"
     }
 });
